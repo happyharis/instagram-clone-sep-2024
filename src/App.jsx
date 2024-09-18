4987792857116786fbb74c3d579dadcf92ac71f3
Haris Samingan <muhdharis1@gmail.com>
2024-09-18 11:14:22 +0800

    ++

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 import { Col, Row } from 'react-bootstrap'
 import IconButton from './components/IconButton'
 import ProfileHeader from './components/ProfileHeader'
+import { createContext } from 'react'
+import { PROFILE_DATA } from "./data";
+
+export const ProfileContext = createContext()
 
 export default function App() {
   return (
-    <Row>
-      <Col sm={1}
-        className='d-flex flex-column justify-content-start align-items-center vh-100 bg-light'
-        style={{ position: 'sticky', top: 0 }}
-      >
-        <IconButton className='bi bi-instagram' isTop></IconButton>
-        <IconButton className='bi bi-house'></IconButton>
-        <IconButton className='bi bi-search'></IconButton>
-        <IconButton className='bi bi-compass'></IconButton>
-        <IconButton className='bi bi-film'></IconButton>
-        <IconButton className='bi bi-chat'></IconButton>
-        <IconButton className='bi bi-heart'></IconButton>
-        <IconButton className='bi bi-plus-square'></IconButton>
-        <IconButton className='bi bi-person-circle'></IconButton>
-        <IconButton className='bi bi-list' isBottom></IconButton>
-      </Col>
-      <Col sm={11}>
-        <ProfileHeader />
-      </Col>
-    </Row>
+    <ProfileContext.Provider value={PROFILE_DATA}>
+      {/* Don't delete the code in the Row */}
+      <Row>
+        <Col sm={1}
+          className='d-flex flex-column justify-content-start align-items-center vh-100 bg-light'
+          style={{ position: 'sticky', top: 0 }}
+        >
+          <IconButton className='bi bi-instagram' isTop></IconButton>
+          <IconButton className='bi bi-house'></IconButton>
+          <IconButton className='bi bi-search'></IconButton>
+          <IconButton className='bi bi-compass'></IconButton>
+          <IconButton className='bi bi-film'></IconButton>
+          <IconButton className='bi bi-chat'></IconButton>
+          <IconButton className='bi bi-heart'></IconButton>
+          <IconButton className='bi bi-plus-square'></IconButton>
+          <IconButton className='bi bi-person-circle'></IconButton>
+          <IconButton className='bi bi-list' isBottom></IconButton>
+        </Col>
+        <Col sm={11}>
+          <ProfileHeader />
+        </Col>
+      </Row>
+    </ProfileContext.Provider>
   )
 }
