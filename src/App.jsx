0024d6e7b2bef64673d84f2a32937f22cc2a989b
Haris Samingan <muhdharis1@gmail.com>
2024-09-18 11:07:17 +0800

    ++

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Col, Row } from 'react-bootstrap'
 import IconButton from './components/IconButton'
+import ProfileHeader from './components/ProfileHeader'
 
 export default function App() {
   return (
@@ -19,7 +20,9 @@ export default function App() {
         <IconButton className='bi bi-person-circle'></IconButton>
         <IconButton className='bi bi-list' isBottom></IconButton>
       </Col>
-      <Col sm={11}>Main</Col>
+      <Col sm={11}>
+        <ProfileHeader />
+      </Col>
     </Row>
   )
 }
