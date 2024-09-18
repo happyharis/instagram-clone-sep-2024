aa2851b10b8a208daa84841e664b4be03d070066
Haris Samingan <muhdharis1@gmail.com>
2024-09-18 10:34:20 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Col, Row } from 'react-bootstrap'
+import IconButton from './components/IconButton'
+
+export default function App() {
+  return (
+    <Row>
+      <Col sm={1}
+        className='d-flex flex-column justify-content-start align-items-center vh-100 bg-light'
+        style={{ position: 'sticky', top: 0 }}
+      >
+        <IconButton className='bi bi-instagram' isTop></IconButton>
+        <IconButton className='bi bi-house'></IconButton>
+        <IconButton className='bi bi-search'></IconButton>
+        <IconButton className='bi bi-compass'></IconButton>
+        <IconButton className='bi bi-film'></IconButton>
+        <IconButton className='bi bi-chat'></IconButton>
+        <IconButton className='bi bi-heart'></IconButton>
+        <IconButton className='bi bi-plus-square'></IconButton>
+        <IconButton className='bi bi-person-circle'></IconButton>
+        <IconButton className='bi bi-list' isBottom></IconButton>
+      </Col>
+      <Col sm={11}>Main</Col>
+    </Row>
+  )
+}
