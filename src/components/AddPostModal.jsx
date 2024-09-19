13772676e9ab3da93da4ad789e73e5cf4e2fd3a0
Haris Samingan <muhdharis1@gmail.com>
2024-09-19 10:24:04 +0800

    add add post modal

--- src/components/AddPostModal.jsx
+++ src/components/AddPostModal.jsx
@@ -1,7 +1,8 @@
 import { useContext, useState } from "react"
 import { ProfileContext } from "../App"
+import { Button, Col, Form, Image, Modal, Row } from "react-bootstrap"
 
-export default function AddPostModal() {
+export default function AddPostModal({ show, handleClose }) {
   const { image, name } = useContext(ProfileContext)
   const [imageUrl, setImageUrl] = useState('')
   const [description, setDescription] = useState('')
@@ -12,7 +13,7 @@ export default function AddPostModal() {
     if (imageUrl) { // if the image url is not empty
       setImageUrl('')
       setDescription('')
-      // handleClose()
+      handleClose()
     } else { // if the image url is empty
       setInvalidUrl(true) // show an error
     }
@@ -22,7 +23,65 @@ export default function AddPostModal() {
     setInvalidUrl(true)
   }
 
+  const handleImageLoad = () => {
+    setInvalidUrl(false)
+  }
+
   return (
-    <div>AddPostModal</div>
+    <Modal show={show} onHide={handleClose} size="lg">
+      <Modal.Header>
+        <Modal.Title>Create new post</Modal.Title>
+      </Modal.Header>
+
+      <Form onSubmit={handleSubmit}>
+        <Modal.Body>
+          <Row>
+
+            <Col sm={7} style={{ margin: '0px' }}>
+              <Image
+                src={imageUrl ? imageUrl : 'https://sig1.co/img-placeholder-1'}
+                alt='uploaded content'
+                onError={handleImageError}
+                onLoad={handleImageLoad}
+                style={{ width: '100%' }}
+              />
+            </Col>
+
+            <Col sm={5}>
+              <Image
+                src={image}
+                alt='uploader'
+                style={{ width: '32px' }}
+                roundedCircle
+              />
+              <span className="ms-3">{name}</span>
+
+              <Form.Control value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+                className="my-3"
+                placeholder="Add image url"
+              />
+
+              {invalidUrl && (
+                <div className="text-danger">
+                  Invalid URL or failed to load image
+                </div>
+              )}
+
+              <Form.Control value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="my-3"
+                as='textarea'
+                rows={3}
+                placeholder="Write a caption..."
+              />
+              <Button type="submit" style={{ width: '100%' }}>Share</Button>
+
+
+            </Col>
+          </Row>
+        </Modal.Body>
+      </Form>
+    </Modal>
   )
 }
