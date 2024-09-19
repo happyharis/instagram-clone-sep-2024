e9ce523649c0ba67294963ca9ee750e50798968b
Haris Samingan <muhdharis1@gmail.com>
2024-09-19 11:49:11 +0800

    Add update

---
+++ src/components/UpdatePostModal.jsx
@@ -0,0 +1,103 @@
+import { useContext, useEffect, useState } from "react"
+import { ProfileContext } from "../App"
+import { Button, Col, Form, Image, Modal, Row } from "react-bootstrap"
+import { useDispatch, useSelector } from "react-redux"
+import { createPost } from "../features/posts/postsSlice"
+
+export default function UpdatePostModal({ show, handleClose, postId }) {
+  const { image, name } = useContext(ProfileContext)
+  const dispatch = useDispatch()
+
+  const post = useSelector((state) => {
+    state.posts.find((post) => post.id === postId)
+  })
+
+  const [imageUrl, setImageUrl] = useState('')
+  const [description, setDescription] = useState('')
+  const [invalidUrl, setInvalidUrl] = useState(false)
+
+  useEffect(() => {
+    if (post) {
+      setImageUrl(post.image)
+      setDescription(post.description)
+    }
+  }, [post])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (imageUrl) { // if the image url is not empty
+      dispatch(createPost({ image: imageUrl, description }))
+      setImageUrl('')
+      setDescription('')
+      handleClose()
+    } else { // if the image url is empty
+      setInvalidUrl(true) // show an error
+    }
+  }
+
+  const handleImageError = () => {
+    setInvalidUrl(true)
+  }
+
+  const handleImageLoad = () => {
+    setInvalidUrl(false)
+  }
+
+  return (
+    <Modal show={show} onHide={handleClose} size="lg">
+      <Modal.Header>
+        <Modal.Title>Create new post</Modal.Title>
+      </Modal.Header>
+
+      <Form onSubmit={handleSubmit}>
+        <Modal.Body>
+          <Row>
+
+            <Col sm={7} style={{ margin: '0px' }}>
+              <Image
+                src={imageUrl ? imageUrl : 'https://sig1.co/img-placeholder-1'}
+                alt='uploaded content'
+                onError={handleImageError}
+                onLoad={handleImageLoad}
+                style={{ width: '100%' }}
+              />
+            </Col>
+
+            <Col sm={5}>
+              <Image
+                src={image}
+                alt='uploader'
+                style={{ width: '32px' }}
+                roundedCircle
+              />
+              <span className="ms-3">{name}</span>
+
+              <Form.Control value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+                className="my-3"
+                placeholder="Add image url"
+              />
+
+              {invalidUrl && (
+                <div className="text-danger">
+                  Invalid URL or failed to load image
+                </div>
+              )}
+
+              <Form.Control value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="my-3"
+                as='textarea'
+                rows={3}
+                placeholder="Write a caption..."
+              />
+              <Button type="submit" style={{ width: '100%' }}>Share</Button>
+
+
+            </Col>
+          </Row>
+        </Modal.Body>
+      </Form>
+    </Modal>
+  )
+}
